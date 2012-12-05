abbec775944c524052c17a6d70aef81144a1ac73
Francois Conil <francoisconil@gmail.com>
2012-12-05 13:52:11 +1100

    Adding script to generate repo information for mariaDB

---
+++ mariadb.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+
+# A quick script to automate what https://downloads.mariadb.org/mariadb/repositories/ does
+# v0.1 centos 6 Proof of concept
+# Author: Francois Conil
+
+from sys import argv
+import os
+import platform
+
+ver = "5.5 or 10.0"
+
+if not len(argv) == 2:
+    print "Usage: python mariadb.py <version> %s" % ver
+    exit(1)
+else:
+    script, version = argv
+    if version == "10" or version == "10.0":    
+        version = "10.0"
+    elif not version == "5.5":
+        print "version expected to be %s" % ver
+        exit(1)
+
+    arch = platform.architecture()[0]
+    dist = platform.linux_distribution()
+
+    
+    repo_dist = dist[0].lower()  
+    if repo_dist in ("centos","redhat", "fedora"):
+        repo_dist = repo_dist + dist[1][0]
+        if arch == "64bit":
+            repo_arch = "amd64"
+        else:
+            repo_arch = "x86"
+   
+        print  
+        f = open("/etc/yum.repos.d/MariaDB.repo",'w') 
+        
+        f.write("[mariadb]")
+        f.write("\n")
+        f.write("name = MariaDB")
+        f.write("\n")
+        f.write("baseurl = http://yum.mariadb.org/%s/%s-%s" % (version, repo_dist, repo_arch))
+        f.write("\n")
+        f.write("gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB")
+        f.write("\n")
+        f.write("gpgcheck=1")
+        f.write("\n")
+        f.close()
+        
+        #import GPG key
+        os.popen("rpm --import http://yum.mariadb.org/RPM-GPG-KEY-MariaDB")
+ 
+    elif repo_dist in ("ubuntu", "debian"):
+        #not tested yet
+        #using australian mirror
+
+        f = open("/etc/apt/sources.list.d/MariaDB.list","w") 
+        f.write("\n")
+        f.write("deb http://mirror.aarnet.edu.au/pub/MariaDB/repo/%s/%s %s main" % (version, repo_dist, dist[2]))
+        f.write("\n")
+        f.write("deb-src http://mirror.aarnet.edu.au/pub/MariaDB/repo/%s/%s %s main" % (version, repo_dist, dist[2]))
+        f.write("\n")
+        f.close()
+        
+        #import GPG key
+        os.popen("apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db")
+
+ 
+    
+    
