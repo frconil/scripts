22c7e951a416f09caa5578e28230bfae630f179a
François Conil <fconil@infoxchange.net.au>
2015-08-05 14:31:37 +1000

    fixing a few things for cronlock

--- cronlock.sh
+++ cronlock.sh
@@ -14,18 +14,20 @@
 # INSERT INTO task_keys (task, last_executed) SELECT '<TASK NAME>', '-INFINITY' WHERE NOT EXISTS (SELECT task FROM task_keys WHERE task = '<TASK NAME>');
 #
 # $DB_DEFAULT_URL is a standard postgres url: postgresql://user:password@server:port/database
+. /root/.cronfile
 
 if [[ $# -lt 2  ]]; then
 	echo "USAGE: $0 TASK_NAME COMMAND ARGS"
 	exit 1
 fi
 
+TASK=$1
 if [[ "$DB_DEFAULT_URL" == ""  ]]; then
 	echo "No postgresql server specified, exiting"
 	exit 1
 else
 #we test we can actually connect
-	psql $DB_DEFAULT_URL -A -c "select 1 from task_keys WHERE = '$TASK';"
+	psql $DB_DEFAULT_URL -A -c "select 1 from task_keys WHERE task = '$TASK';"
 	if [[ "$?" -ne "0" ]]; then
 		echo "Could not connect to postgresql server specified, table doesn't exist or task has never been initialised, exiting"
 		exit 1
@@ -33,7 +35,6 @@ else
 fi
 
 # we set up task and pop the first argument to keep the command as $@
-TASK=$1
 shift
 LOCK=`psql $DB_DEFAULT_URL -c "UPDATE task_keys SET last_executed = current_timestamp, by_worker_id=$$ WHERE task = '$TASK' AND last_executed < (current_timestamp - INTERVAL '1' MINUTE) RETURNING *;"  | grep UPDATE`
 
